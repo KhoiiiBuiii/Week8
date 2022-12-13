7209243c626e66eb251f3789494af2a554046039
KhoiiiBuiii <92972417+KhoiiiBuiii@users.noreply.github.com>
2022-12-13 15:53:13 +0700

    Question 2 commit

--- src/Question1.java
+++ src/Question1.java
@@ -1,6 +1,7 @@
 
 import java.util.Scanner;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Question1 {
    public static void main(String[] args) throws IOException {
@@ -17,9 +18,28 @@ public class Question1 {
        pw.println(name + ","+ address+","+ age);
        pw.flush();
        pw.close();
+       // exercise 2
+       Scanner fileScanner = new Scanner(new File("src\\user.txt"));
+       String line;
+       double sum = 0;
+       int count = 0;
 
+       while (fileScanner.hasNext()){
+           count++;
+           line = fileScanner.nextLine();
+           StringTokenizer reader = new StringTokenizer(line, ",");
 
+           if (reader.countTokens() !=3){
+               throw  new IOException("Invalid input format!");
+           } else {
+               name = reader.nextToken();
+               address = reader.nextToken();
+               age = Integer.parseInt(reader.nextToken());
+               sum += age;
+           }
+           System.out.println(name + " Lives at " +address+ " and is " +age+ " year old");
 
+       }
     }
 
 }
